2e81c7b7492945ffa0e3dfb015f9cb83bd08eaf0
Cuong Ho <cuong.ict.10101@gmail.com>
2018-07-07 23:06:36 +0700

    Fix port

--- app.js
+++ app.js
@@ -3,12 +3,13 @@ const bodyParser = require('body-parser');
 const routes = require('./routes/routes.js')
 
 const app = express();
+const port = process.env.PORT || 4000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 routes(app);
 
-var server = app.listen(8080, function() {
+var server = app.listen(port, function() {
     console.log('app running on port.', server.address().port);
 });
