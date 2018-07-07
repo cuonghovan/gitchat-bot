4f988b30b420bd74d40fa3a559aea8682fc4cae2
Cuong Ho <cuong.ict.10101@gmail.com>
2018-07-07 23:15:34 +0700

    View github's request

--- routes/routes.js
+++ routes/routes.js
@@ -1,6 +1,10 @@
 const appRouter = function(app) {
-    app.get("/test", function(req, res) {
+    app.get("/", function(req, res) {
         res.status(200).send("Welcome to our restful API");
+    });
+
+    app.post("/webhook", function(req, res) {
+        res.send(req);
     });
 };
 
